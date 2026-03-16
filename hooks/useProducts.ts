010334adeee35b316f757f02c5c4720534fc4a22
Nitro <nitro@videoautomation.local>
2026-03-16 18:10:31 -0300

    feat(admin): update products pagination default to 12 and options to 12/24/36

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -65,7 +65,7 @@ export const useProducts = () => {
 
     // Pagination
     const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(24);
+    const [itemsPerPage, setItemsPerPage] = useState(12);
 
     /**
      * Fetch products from Supabase.
